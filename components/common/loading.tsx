1eca4959ea3d5d970cc5b3a4d6a31eae4b0e6107
rosylo <rosylo@nine-yi.com>
2022-01-28 14:21:01 +0800

    feat: reusable component - loading

---
+++ components/common/loading.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+import { CircularProgress, Backdrop} from '@mui/material';
+
+
+type LoadingProps = {
+    isLoading: boolean
+}
+
+const Loading:FC<LoadingProps> = ({isLoading}) => {
+
+return (
+    <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={isLoading}
+  >
+    <CircularProgress color="inherit" />
+  </Backdrop>
+)
+}
+
+export default Loading;
