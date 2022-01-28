c81b4bd7b74754eec5c039e907b1c3a00d205000
rosylo <rosylo@nine-yi.com>
2022-01-28 14:25:23 +0800

    feat: add theme color and typography

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,27 @@ import type { AppProps, AppContext } from 'next/app';
 
 const theme = createTheme({
   palette: {
-    primary: { main: '#ff5044', },
+    primary: { main: '#ff5044', contrastText: '#ffffff'},
     secondary: { main: '#626262', },
     error: { main: '#ff1100', },
+    info: {
+      main: '#ffffff'
+    },
   },
+  typography: {
+    h3: {
+      fontWeight: 800,
+      fontSize: '1.25rem',
+    },
+    body1: {
+      fontWeight: 600,
+      fontSize: '1rem',
+    },
+    body2: {
+      fontWeight: 400,
+      fontSize: '0.875rem',
+    }
+  }
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
